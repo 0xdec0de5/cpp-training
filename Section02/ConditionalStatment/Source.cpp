532aac8d6f9df7e4946cb740a7fb6877ccb1831b
Trent Millar <trent.millar@gmail.com>
2018-02-28 15:42:16 -0700

    pushing ConditionalStatment demo up to github

---
+++ Section02/ConditionalStatment/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int i = 0;
+
+	std::cout << "Please enter a number  ";
+	std::cin >> i;
+
+	if (i > 0)
+	{
+		std::cout << std::endl << 
+			"You entered a number greater than 0";
+	}
+	else
+	{
+		std::cout << std::endl << 
+			"You entered a number less than or equal 0";
+	}
+	return 0;
+}
